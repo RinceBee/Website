067b40c57e297bb978aad120eecda775cafd46d8
RinceBee <kumar.baral57@gmail.com>
2022-06-13 21:52:36 +0545

    modified at 13th june

--- mail.php
+++ mail.php
@@ -3,10 +3,11 @@
 
 $name = $_POST['name'];
 $email= $_POST['email'];
+$number= $_POST['number'];
 $message= $_POST['message'];
 $to = "kumar.baral57@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n  Mobile = " . $number . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@kumarbaral.com.np" . "\r\n" .
 "CC: kumar.baral572@gmail.com";
 if($email!=NULL){
